c48a5b32ff36a4d32ee95dde52dc6175a5715f3f
davisn7178 <157392462+davisn7178@users.noreply.github.com>
2024-01-22 16:16:59 +0000

    completed M1T2

---
+++ M1T2_Davis.cpp
@@ -0,0 +1,24 @@
+/*
+CSC 134
+M1T2
+Nathan Davis
+1/22/24
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Introduction
+    cout <<
+    "Hello World!\n"
+    "I'm Nathan Davis.\n"
+    "I enjoy listening to music.\n";
+
+    //Ask a question
+    string name;
+    cout << "What's your name? ";
+    cin >> name;
+    cout << "Nice to meet you, " << name << "!\n\n\n";
+    return 0;
+}
