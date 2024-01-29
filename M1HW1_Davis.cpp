279a1ff6c79a4d5fb6d330c0ef17028f26537129
davisn7178 <157392462+davisn7178@users.noreply.github.com>
2024-01-29 15:23:15 +0000

    Created M1HW1

---
+++ M1HW1_Davis.cpp
@@ -0,0 +1,23 @@
+/*
+CSC 134
+M1HW1
+Nathan Davis
+1/29/2024
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Declare variables
+    string movie = "Pirates of the Caribbean: The Curse of the Black Pearl";
+    int year = 2003;
+    double gross = 654.3;
+
+    cout << "I choose the movie " << movie << 
+    ",\nwhich came out in the year " << year << 
+    " and grossed a total box office income of " << gross << " million dollars.\n";
+    cout << "I saw this movie when I was really young and the scene that I remember the most\nis the beginning where Jack Sparrow arrives at the port on a small sinking ship.\n\n";
+
+    return 0;
+}
