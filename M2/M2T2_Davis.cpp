944a2329c0af60e4118147252f9ea2311b75aa49
davisn7178 <157392462+davisn7178@users.noreply.github.com>
2024-02-11 18:44:38 +0000

    created m2t2

---
+++ M2/M2T2_Davis.cpp
@@ -0,0 +1,17 @@
+/*
+CSC 134
+M2T1 - Receipt
+Nathan Davis
+2/11/2024
+*/
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    cout << string( 5, '\n' );
+    return 0;
+}
