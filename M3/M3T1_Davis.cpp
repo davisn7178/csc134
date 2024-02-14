8c409cbc82c6e810c71dda62b102149c8c0885c4
davisn7178 <157392462+davisn7178@users.noreply.github.com>
2024-02-14 15:12:03 +0000

    created m3t1

---
+++ M3/M3T1_Davis.cpp
@@ -0,0 +1,19 @@
+/*
+CSC 134
+M3T1
+Nathan Davis
+2/14/2024
+*/
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    
+    
+
+    cout << string( 5 ,'\n' );
+    return 0;
+}
